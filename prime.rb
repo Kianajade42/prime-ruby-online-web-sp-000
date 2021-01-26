3b1eae3af59736848f66ff2c68c0233e64fb04ea
Kiana Long <kianajade42.kl@gmail.com>
2021-01-26 16:12:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(number)
  if number < 0 or number == 0 or number == 1
    return false
  else
-   (2..number-1).to_a.all? do |possible_factor|
-     number % possible_factor != 0
+   (2..number-1).to_a.all? do |integer|
+     number % integer != 0
    end
  end
 end
